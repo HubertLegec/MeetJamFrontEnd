1512646150eff10b5bc4329d9e356e2077dad257
Marcin Jarząbek <marcin.jarzabek.pl@gmail.com>
2016-04-12 14:02:08 +0200

    sign up success scenario, error feedback from server

--- app/account/signup/signup.controller.js
+++ app/account/signup/signup.controller.js
@@ -10,8 +10,30 @@
     function SignUpController(SignUpService) {
         var vm = this;
 
+        vm.errorUserExists = null;
+        vm.errorInvalidEmail = null;
+        vm.errorInvalidPassword = null;
+        vm.success = null;
+
         vm.signUp = function () {
-            SignUpService.register(vm.username, vm.email, vm.password);
+            vm.errorUserExists = null;
+            vm.errorInvalidEmail = null;
+            vm.errorInvalidPassword = null;
+            vm.success = null;
+
+            SignUpService.register(vm.username, vm.email, vm.password)
+                .then(function successCallback(response) {
+                    vm.success = 'OK';
+                }, function errorCallback(response) {
+                    if(response.status === 406) {
+                        if(response.data.messages.indexOf('User with given login already exists!') != -1)
+                            vm.errorUserExists = 'ERROR';
+                        if(response.data.messages.indexOf('Email is invalid!') != -1)
+                            vm.errorInvalidEmail = 'ERROR';
+                        if(response.data.messages.indexOf('Password do not match requirements!') != -1)
+                            vm.errorInvalidPassword = 'ERROR';
+                    }
+                });
         }
     }
 
